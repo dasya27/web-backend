54be8c58e14fc96946a6763abc268dc0a0e79268
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-05-08 10:53:03 +0300

    Update index.php

--- practice3/index.php
+++ practice3/index.php
@@ -7,48 +7,70 @@
     <title>web3</title>
 
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+
+
 </head>
 <body>
     <form action="form.php" method="post">
-        <div class="title">Form</div>
-        <input type="text" name="name" placeholder="Your name" class="text"><br>
-        <input type="email" name="mail" placeholder="Your email" class="text"><br>
-
-        <select name="year"class="year">
-            <option value="">Choose year</option>
-            <?php
-            for ($year = 1990; $year < 2023; $year++) {
-                echo '<option '.$selected.' value="'.$year.'">'.$year.'</option>';
-            }
-            ?>
-        </select><br>
-
-        <div class="sex">
-            You are:
-            <input type="radio" name="sex" value="man">
-            <label for="man">Man</label>
-            <input type="radio" name="sex" value="woman">
-            <label for="woman">Woman</label> <br>
+        <div class="left">
+            <img src="humming-bird.png" alt="">
         </div>
-
-        <div class="limbs">
-            Limbs:
-            <input type="radio" name="limbs" value="1">
-            <label for="1">1</label>
-            <input type="radio" name="limbs" value="2">
-            <label for="2">2</label>
-            <input type="radio" name="limbs" value="3">
-            <label for="3">3</label>
-            <input type="radio" name="limbs" value="4">
-            <label for="4">4</label><br>
+        <div class="right">
+        <div class="title">Contact us</div>
+        <div class="title-name">
+            Your name
         </div>
+        <input type="text" name="name" class="text"><br>
+        <div class="title-mail">
+            Your email address
+        </div>
+        <input type="email" name="mail" class="text"><br>
 
-        <select multiple class="skills" name="skills">
-            <option value="">Your skills</option>
-            <option value="kind">Kind</option>
-            <option value="honest">Honest</option>
-            <option value="funny">Funny</option>
-        </select><br>
+        <div class="sub">
+            <div class="sub-left">
+                <select name="year"class="year" >
+                <option selected disabled hidden>Choose year</option>
+                <?php
+                for ($year = 2004; $year < 2023; $year++) {
+                    echo '<option '.$selected.' value="'.$year.'">'.$year.'</option>';
+                }
+                ?>
+                </select><br>
+
+                <div class="sex">
+                    <div class="sex-text">You are:</div>
+                    <input type="radio" name="sex" value="man">
+                    <label for="man">Man</label>
+                    <input type="radio" name="sex" value="woman">
+                    <label for="woman">Woman</label> <br>
+                </div>
+
+                <div class="limbs">
+                    Limbs:
+                    <input type="radio" name="limbs" value="1">
+                    <label for="1">1</label>
+                    <input type="radio" name="limbs" value="2">
+                    <label for="2">2</label>
+                    <input type="radio" name="limbs" value="3">
+                    <label for="3">3</label>
+                    <input type="radio" name="limbs" value="4">
+                    <label for="4">4</label><br>
+                </div>
+            </div>
+            <div class="sub-right">
+                <div class="title-skills">
+                    Skills:
+                </div>
+                <select multiple class="skills" name="skills[]">
+                    <option value="1">Kind</option>
+                    <option value="2">Honest</option>
+                    <option value="3">Funny</option>
+                </select><br>
+            </div>
+        </div>
 
         <br>
         <textarea rows="4" placeholder="Your bio"></textarea><br>
@@ -56,7 +78,8 @@
         <input type="checkbox" name="agree" value="yes" class="agree">
         <label for="agree" >I agree</label><br>
 
-        <input type="submit" class="btn" value="Отправить" />
+        <input type="submit" class="btn" value="Submit" />
+        </div>
     </form>
 
 </body>
