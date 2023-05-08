c34ec527a5e05d0b9da949f76dad91f9c5af7088
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-05-08 10:52:21 +0300

    Update db.sql

--- practice3/db.sql
+++ practice3/db.sql
@@ -1,20 +1,21 @@
 CREATE TABLE Applications (
-    id int unsigned NOT NULL AUTO_INCREMENT,
+    id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(128) NOT NULL DEFAULT '',
     year int NOT NULL DEFAULT 0,
     mail VARCHAR(255) NOT NULL DEFAULT '',
     sex varchar(5) NOT NULL DEFAULT '',
     limbs int NOT NULL DEFAULT 0,
-    biography varchar(255) NOT NULL DEFAULT '',
-    PRIMARY KEY (id)
+    biography varchar(255) NOT NULL DEFAULT ''
 )
 
 CREATE TABLE Applications_skills (
     id int unsigned NOT NULL,
-    skill_id int unsigned NOT NULL
+    skill_id int unsigned NOT NULL,
+    FOREIGN KEY (id) REFERENCES Applications(id),
+    FOREIGN KEY (skill_id) REFERENCES Skills(skill_id)
 );
 
 CREATE TABLE Skills (
-    id int unsigned NOT NULL,
+    skill_id int unsigned NOT NULL PRIMARY KEY,
     skill_name varchar(128) NOT NULL
 );
