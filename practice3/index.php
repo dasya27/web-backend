e9084d1e7744f30cae3c5225bd336ac5940c0c69
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-05-09 13:46:11 +0300

    Update index.php

--- practice3/index.php
+++ practice3/index.php
@@ -23,26 +23,26 @@
         <div class="title-name">
             Your name
         </div>
-        <input type="text" name="name" class="text"><br>
+        <input type="text" name="name" class="text" required><br>
         <div class="title-mail">
             Your email address
         </div>
-        <input type="email" name="mail" class="text"><br>
+        <input type="email" name="mail" class="text" required><br>
 
         <div class="sub">
             <div class="sub-left">
-                <select name="year"class="year" >
-                <option selected disabled hidden>Choose year</option>
-                <?php
-                for ($year = 2004; $year < 2023; $year++) {
-                    echo '<option '.$selected.' value="'.$year.'">'.$year.'</option>';
-                }
-                ?>
+                <select name="year" class="year" required>
+                    <option value="">Choose year</option>
+                    <?php
+                        for ($year = 2004; $year < 2023; $year++) {
+                            echo '<option '.$selected.' value="'.$year.'">'.$year.'</option>';
+                        }
+                    ?>
                 </select><br>
 
                 <div class="sex">
                     <div class="sex-text">You are:</div>
-                    <input type="radio" name="sex" value="man">
+                    <input type="radio" name="sex" value="man" checked>
                     <label for="man">Man</label>
                     <input type="radio" name="sex" value="woman">
                     <label for="woman">Woman</label> <br>
@@ -50,21 +50,19 @@
 
                 <div class="limbs">
                     Limbs:
-                    <input type="radio" name="limbs" value="1">
-                    <label for="1">1</label>
-                    <input type="radio" name="limbs" value="2">
-                    <label for="2">2</label>
-                    <input type="radio" name="limbs" value="3">
-                    <label for="3">3</label>
-                    <input type="radio" name="limbs" value="4">
-                    <label for="4">4</label><br>
+                    <input type="radio" name="limbs" value="4" checked>
+                    <label for="4">4</label>
+                    <input type="radio" name="limbs" value="6">
+                    <label for="6">6</label>
+                    <input type="radio" name="limbs" value="8">
+                    <label for="8">8</label><br>
                 </div>
             </div>
             <div class="sub-right">
                 <div class="title-skills">
                     Skills:
                 </div>
-                <select multiple class="skills" name="skills[]">
+                <select multiple class="skills" name="skills[]" required>
                     <option value="1">Kind</option>
                     <option value="2">Honest</option>
                     <option value="3">Funny</option>
@@ -73,10 +71,10 @@
         </div>
 
         <br>
-        <textarea rows="4" placeholder="Your bio" name="bio"></textarea><br>
+        <textarea rows="4" placeholder="Your bio" name="bio" required></textarea><br>
 
-        <input type="checkbox" name="agree" value="yes" class="agree">
-        <label for="agree" >I agree</label><br>
+        <input type="checkbox" name="agree" value="yes" class="agree" required>
+        <label for="agree" >I am familiar with the contract</label><br>
 
         <input type="submit" class="btn" value="Submit" />
         </div>
