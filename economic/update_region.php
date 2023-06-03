a09daab3e4281323333e05d32a691ed2ae16c01b
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-03 22:33:35 +0300

    Update update_region.php

--- economic/update_region.php
+++ economic/update_region.php
@@ -1,8 +1,27 @@
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
 
 <div class="right-button-margin">
     <a href="regions.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
 </div>
+
+
+<?php
+    include('connection.php');
+    $conn = connect();
+
+    $name = $_POST['name'];
+    $center = $_POST['center'];
+
+    //подготавливаем запрос
+    $query = $conn->prepare("INSERT INTO regions (name, center) VALUES(?, ?)");
+    $mass = array($name, $center);
+    $query->execute($mass);
+
+    header('Location: regions.php');
+
+?>
 
 <?php 
     include('connection.php');
