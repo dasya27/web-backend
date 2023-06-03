139d7cb97ee1e09425022135efa9484c52d42f95
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-03 21:27:17 +0300

    Update insert_region.php

--- economic/insert_region.php
+++ economic/insert_region.php
@@ -2,6 +2,9 @@
     include('connection.php');
     $conn = connect();
 
+    $name = $_POST['name'];
+    $center = $_POST['center'];
+
     //подготавливаем запрос
     $query = $conn->prepare("INSERT INTO regions (name, center) VALUES(?, ?)");
     $mass = array($name, $center);
