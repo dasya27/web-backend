81c629df672fae41662906d08e64799f64999c99
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-03 21:54:44 +0300

    Update regions.php

--- economic/regions.php
+++ economic/regions.php
@@ -1,11 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 
-<?php 
-    include('connection.php');
-    $conn = connect();
-?>
-
 <body>
     <header>
         <a href="index.php" class="logo">economic</a>
@@ -30,6 +25,9 @@
 </body>
 
 <?php
+    include('connection.php');
+    $conn = connect();
+
     //вывод записей в таблицу
     $query = "SELECT * FROM regions";
     $stmt = $conn->prepare($query);
