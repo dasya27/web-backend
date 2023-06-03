76abc6751ffd502f96af9e1ab231f12f0e0f10dd
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-03 21:22:58 +0300

    Update index.php

--- economic/index.php
+++ economic/index.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>economic db</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href="index.php" class="logo">economic</a>
+        <div class="menu">
+            <a href="regions.php" class="item">Regions</a>
+            <a href="develop.php" class="item">Develop</a>
+            <a href="statistics.php" class="item">Statistics</a>
+            <a href="fields.php" class="item">Fields</a>
+        </div>
+    </header>
+
+    <h1>Вы можете редактировать, добавлять и удалять записи в базе данных</h1>
+
+    <div class="btns">
+        <a href="regions.php" class="btn btn-primary">Regions</a>
+        <a href="develop.php" class="btn btn-primary">Develop</a>
+        <a href="statistics.php" class="btn btn-primary">Statistics</a>
+        <a href="fields.php" class="btn btn-primary">Fields</a>
+    </div>
+</body>
+</html>
