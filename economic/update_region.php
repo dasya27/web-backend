26f60cd4a7e17954355757a361a3680bddbcf017
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-03 22:39:14 +0300

    Update update_region.php

--- economic/update_region.php
+++ economic/update_region.php
@@ -5,23 +5,6 @@
     <a href="regions.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
 </div>
 
-
-<?php
-    include('connection.php');
-    $conn = connect();
-
-    $name = $_POST['name'];
-    $center = $_POST['center'];
-
-    //подготавливаем запрос
-    $query = $conn->prepare("INSERT INTO regions (name, center) VALUES(?, ?)");
-    $mass = array($name, $center);
-    $query->execute($mass);
-
-    header('Location: regions.php');
-
-?>
-
 <?php 
     include('connection.php');
     $id = isset($_GET["id"]) ? $_GET["id"] : die("ERROR: отсутствует ID.");
