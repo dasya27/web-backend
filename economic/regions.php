beaa31cea0ddc9ea23b252716f9ddd8fe0739e67
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 10:32:35 +0300

    Update regions.php

--- economic/regions.php
+++ economic/regions.php
@@ -1,5 +1,4 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+
 
 <!-- jQuery (необходим для Bootstrap JavaScript) -->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
@@ -10,7 +9,8 @@
 <!-- bootbox JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
 
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
 
 </body>
 
