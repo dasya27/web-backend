fcb6f6159b971696f058a61a43945a7d857864c8
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 23:29:43 +0300

    Update delete_field.php

--- economic/delete_field.php
+++ economic/delete_field.php
@@ -4,6 +4,13 @@
         $id = $_GET["id"];
         $conn = connect();
 
+        //удаляем из отраслей и регионов
+        $query = "DELETE FROM fields_regions WHERE id_field = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+
+        //удаляем из отраслей
         $query = "DELETE FROM fields WHERE id = ?";
         $stmt = $conn->prepare($query);
         $stmt->bindParam(1, $id);
