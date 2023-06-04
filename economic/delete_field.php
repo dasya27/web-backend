c3af44a03ee099e7c4ba5799493c5edb9f385696
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 22:08:59 +0300

    Create delete_field.php

---
+++ economic/delete_field.php
@@ -0,0 +1,13 @@
+<?php
+    //подключаемся к базе данных
+        include_once('connection.php');
+        $id = $_GET["id"];
+        $conn = connect();
+
+        $query = "DELETE FROM field WHERE id = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+
+        header('Location: field.php');
+?>
