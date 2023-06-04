29affb2b38e569e059baa6e9e24d4c3a90371870
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-05 01:59:14 +0300

    Create delete_statistics.php

---
+++ economic/delete_statistics.php
@@ -0,0 +1,13 @@
+<?php
+    //подключаемся к базе данных
+        include_once('connection.php');
+        $id = $_GET["id_region"];
+        $conn = connect();
+
+        $query = "DELETE FROM statistics WHERE id_region = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+
+        header('Location: regions.php');
+?>
