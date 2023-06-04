c91500f23f58713e332c50a5ff30db819230c393
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 10:25:15 +0300

    Update regions.php

--- economic/regions.php
+++ economic/regions.php
@@ -6,10 +6,11 @@
 
 <!-- bootbox JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
-</body>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 
+</body>
 
 <?php 
     include('connection.php');
