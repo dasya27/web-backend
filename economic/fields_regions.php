bcf3fa149150fbe515d7e4e901e513fe73ef549f
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-05 00:18:45 +0300

    Update fields_regions.php

--- economic/fields_regions.php
+++ economic/fields_regions.php
@@ -82,7 +82,7 @@
                 echo "<td>";
                     // здесь будут кнопки для просмотра, редактирования и удаления
                     echo "
-                    <a href='update_fields_regions.php?id_region={$id_region}' class='btn btn-info left-margin'>
+                    <a href='update_fields_regions.php?id_region={$id_region}' class='btn btn-success left-margin'>
                     <span class='glyphicon glyphicon-edit'></span> Редактировать
 
                     <a href='delete_fields_regions.php?id_region={$id_region}' class='btn btn-danger delete-object'>
