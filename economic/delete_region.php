2295c01409b9863e3dd684f79b2fbcbb8aab7132
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 15:39:12 +0300

    Update delete_region.php

--- economic/delete_region.php
+++ economic/delete_region.php
@@ -1,14 +1,15 @@
 <?php
     //подключаемся к базе данных
-    include('connection.php');
-    $conn = connect();
-    //принимаем этот параметр
-    $id = $_GET['id'];
+    if($_GET) {
+        include('connection.php');
+        $conn = connect();
+        //принимаем этот параметр
+        $id = $_GET['id'];
 
-    $query = "DELETE FROM regions WHERE id = ?";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(1, $id);
-    $stmt->execute();
+        $query = "DELETE FROM regions WHERE id = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
 
-    header("location:javascript://history.go(-1)");
+    }
 ?>
