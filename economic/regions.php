dc37428d1fe205f65a9bac2a6275e9a5ce779b3c
dasya27 <113109207+dasya27@users.noreply.github.com>
2023-06-04 10:06:45 +0300

    Update regions.php

--- economic/regions.php
+++ economic/regions.php
@@ -27,6 +27,38 @@
         </div>
         <input type="submit" class="btn btn-warning" value="add" />
     </form>
+    <script>
+$(document).on("click", ".delete-object", function() {
+        const id = $(this).attr("delete-id");
+
+        bootbox.confirm({
+            message: "<h4>Вы уверены?</h4>",
+            buttons: {
+                confirm: {
+                    label: "<span class='glyphicon glyphicon-ok'></span> Да",
+                    className: "btn-danger"
+                },
+                cancel: {
+                    label: "<span class='glyphicon glyphicon-remove'></span> Нет",
+                    className: "btn-primary"
+                }
+            },
+            callback: function(result) {
+                if (result == true) {
+                    $.post("delete_region.php", {
+                        object_id: id
+                    }, function(data) {
+                        location.reload();
+                    }).fail(function() {
+                        alert("Невозможно удалить.");
+                    });
+                }
+            }
+        });
+
+        return false;
+    });
+</script>
 </body>
 
 <?php
